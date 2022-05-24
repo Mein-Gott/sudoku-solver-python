56cf76577f0e90a3112775cfc2d9d292631d168a
Mein-Gott <eino.m.metso@gmail.com>
2022-05-24 09:06:03 +0300

    candidates_per_cell ready without testing

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 It's important to know a few terms before diving into the code:
 - `cell` = the smallest square with the number
 - `region` = a 3x3 sub-grid
-- `given` = an initially defined number
+- `clue` = an initially defined number
 - `group` = a row, a column or a region
 - `candidate` = a possible value for a cell 
 - `single` = the only candidate for a cell
--- candidates_per_cell.py
+++ candidates_per_cell.py
@@ -1,8 +1,9 @@
 from split_into_groups import split_into_groups
+from copy import deepcopy
 
 def candidates_per_cell(grid):
-    candidates = __generate_default_candidates()
-    candidates = delete_candidates_from_givens(candidates, grid)
+    candidates = generate_default_candidates()
+    candidates = delete_candidates_from_clue_cells(candidates, grid)
 
     groupsets = split_into_groups(grid)
     candidates_in_groupsets = split_into_groups(candidates)
@@ -13,33 +14,33 @@ def candidates_per_cell(grid):
     return candidates
 
 
-def delete_candidates_from_givens(candidates, grid):
-    edited_candidates = candidates.copy()
-    for candidate_row, row in zip(candidates, grid):
-        edited_candidates = delete_candidates_from_givens_row(candidate_row, row)
+def delete_candidates_from_clue_cells(candidates, grid):
+    edited_candidates = deepcopy(candidates)
+    for row_index, row in enumerate(grid):
+        edited_candidates[row_index] = delete_candidates_from_clue_cell_row(candidates[row_index], row)
 
     return edited_candidates
 
-def delete_candidates_from_givens_row(candidate_row, row):
-    edited_candidate_row = candidate_row.copy()
+def delete_candidates_from_clue_cell_row(candidate_row, row):
+    edited_candidate_row = deepcopy(candidate_row)
 
-    for cell_candidates, cell in zip(edited_candidate_row, row):
-        if cell == 0: 
-            cell_candidates = []
+    for cell_index, cell in enumerate(row):
+        if cell != 0: 
+            edited_candidate_row[cell_index] = []
 
     return edited_candidate_row
 
 
 def eliminate_candidates_checking_groupset(groupset_candidates, groupset):
-    edited_groupset_candidates = groupset_candidates.copy()
+    edited_groupset_candidates = deepcopy(groupset_candidates)
 
     for group_index, group in enumerate(groupset):
-        edited_groupset_candidates = eliminate_candidates_group(groupset_candidates[group_index], group)
+        edited_groupset_candidates[group_index] = eliminate_candidates_group(groupset_candidates[group_index], group)
 
     return edited_groupset_candidates
 
 def eliminate_candidates_group(group_candidates, group):
-    edited_group_candidates = group_candidates.copy()
+    edited_group_candidates = deepcopy(group_candidates)
 
     for cell in group:
         edited_group_candidates = eliminate_value_from_group_candidates(edited_group_candidates, cell)
@@ -47,7 +48,7 @@ def eliminate_candidates_group(group_candidates, group):
     return edited_group_candidates
 
 def eliminate_value_from_group_candidates(group_candidates, value):
-    edited_group_candidates = group_candidates.copy()
+    edited_group_candidates = deepcopy(group_candidates)
 
     for candidates_of_cell in edited_group_candidates:
         if value in candidates_of_cell:
@@ -58,19 +59,19 @@ def eliminate_value_from_group_candidates(group_candidates, value):
 
 
 
-def __generate_default_candidates():
+def generate_default_candidates():
     default_candidates = []
     for i in range(9):
-        default_candidates.append(__generate_default_row())
+        default_candidates.append(__generate_default_row_candidates())
     return default_candidates
 
-def __generate_default_row():
+def __generate_default_row_candidates():
     default_row = []
     for i in range(9):
-        default_row.append(__generate_default_cell())
+        default_row.append(__generate_default_cell_candidates())
     return default_row
 
-def __generate_default_cell():
+def __generate_default_cell_candidates():
     default_cell = []
     for i in range(1, 10):
         default_cell.append(i)
